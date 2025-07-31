1b8737e74caffbcce6b0aa4e946f76045acc8934
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-07-31 07:48:36 +0300

    module3-task1. Создан отдельный компонент для страницы Not found.

---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function NotFound(): JSX.Element {
+  return (
+    <Fragment>
+      <h1>
+        404.
+        <br />
+        <small>Page not found.</small>
+      </h1>
+      <Link to="/">Go to main page.</Link>
+    </Fragment>
+  );
+}
+
+export default NotFound;
