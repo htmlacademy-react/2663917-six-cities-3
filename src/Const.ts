8c1d534dab733a275adf6d3f91f299f4eb430426
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-07-31 07:52:03 +0300

    module3-task1. Описаны маршруты в приложении.

---
+++ src/Const.ts
@@ -0,0 +1,11 @@
+export const Settings = {
+  PlacesToStayNumber: 5
+};
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
+
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../Const';
 import Main from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import NotFound from '../../pages/not-found/not-found';
+import Offer from '../../pages/offer/offer';
 
 type AppProps = {
     placesToStayNumber: number;
@@ -6,7 +12,30 @@ type AppProps = {
 
 function App({placesToStayNumber}: AppProps): JSX.Element {
   return (
-    <Main placesToStayNumber={placesToStayNumber} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main placesToStayNumber={placesToStayNumber} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element=<Favorites />
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
