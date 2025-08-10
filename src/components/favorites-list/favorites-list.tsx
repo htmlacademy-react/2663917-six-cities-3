e8016201bb0317dc6c227fdbda859b284773e119
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-08-10 18:00:13 +0300

    module4-task1. На страницу Favorites через props переданы избранные предложения об аренде, сделала отрисовка карточек, переданных в props.

---
+++ src/components/favorites-list/favorites-list.tsx
@@ -0,0 +1,40 @@
+import { Offers } from '../../types/offer';
+import FavoriteOfferCard from '../favorite-offer-card/favorite-offer-card';
+
+type FavoritesListProps = {
+    offers: Offers;
+}
+
+function FavoritesList({offers}: FavoritesListProps): JSX.Element {
+  const groupByCountry = (offers: Offers): Record<string, Offers> => {
+    return offers.reduce((acc, offer) => {
+      const country = offer.country;
+      if (!acc[country]) {
+        acc[country] = [];
+      }
+      acc[country].push(offer);
+      return acc;
+    }, {} as Record<string, Offers>);
+  }
+  
+  return (
+    <ul className="favorites__list">
+      {Object.entries(groupByCountry(offers)).map(([country, offers]) => (
+        <li className="favorites__locations-items" key={country}>
+          <div className="favorites__locations locations locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>{country}</span>
+              </a>
+            </div>
+          </div>
+          <div className="favorites__places">
+            {offers.map((offer) => <FavoriteOfferCard offer={offer} key={offer.id} />)}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default FavoritesList;
