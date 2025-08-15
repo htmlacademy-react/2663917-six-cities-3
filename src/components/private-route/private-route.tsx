c0e75f9c6ca86fad614bd9629206b8a198cf1e5a
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-08-15 13:55:37 +0300

    module5-task1. Добавлены тестовые данные для объектов аренды.

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,5 +1,5 @@
 import {Navigate} from 'react-router-dom';
-import { AppRoute, AuthorizationStatus } from '../../Const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
