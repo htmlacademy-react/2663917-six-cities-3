2f957755659bdb337985903d48dede875d26c0e4
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-11-12 01:27:46 +0300

    module9-task2. Добавлены тесты для компонентов с Link.

--- src/services/process-error-handle.ts
+++ src/services/process-error-handle.ts
@@ -3,6 +3,9 @@
 // import {setError} from '../store/app-process/app-process';
 
 export const processErrorHandle = (message: string): void => {
+  // eslint-disable-next-line no-console
+  console.log(message);
+
   // store.dispatch(setError(message));
   // store.dispatch(clearErrorAction());
 };
